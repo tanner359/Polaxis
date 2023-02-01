ac8b76be729737ddf783b0a57f246d4247f91b60
Tanner Cobb <tannercobb359@gmail.com>
2023-02-01 12:41:18 -0500

    Sound FX Added and UI Updates

---
+++ Polaxis_Game/Assets/Scripts/Static_Animator.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Static_Animator : MonoBehaviour
+{
+    public Sprite[] images;
+    
+    public float speed;
+
+    private SpriteRenderer srTarget;
+    private Image imgTarget;
+
+    private void Awake(){
+        if(gameObject.TryGetComponent(out SpriteRenderer sr)){
+            srTarget = sr;
+        }
+        else if(gameObject.TryGetComponent(out Image img)){
+            imgTarget = img;
+        }
+    }
+
+    private int i;
+    private float t;
+
+    private void LateUpdate(){
+        t += Time.deltaTime;
+        if(t>=speed){
+            if(srTarget != null){
+                srTarget.sprite = images[i];
+                i = i != images.Length-1 ? i+=1 : 0;
+            }
+            else{
+                imgTarget.sprite = images[i];
+                i = i != images.Length-1 ? i+=1 : 0;
+            }
+            t = 0;
+        }
+    }
+}
